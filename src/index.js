f361c0e97601737223b437d08b5f2b15c75b92db
nevena31 <125161542+nevena31@users.noreply.github.com>
2023-12-12 19:51:16 +0100

    Added the API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings:
-      "The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "aa8be038534a124at608fo4af5b10b9a";
+  let context =
+    "You are an inspirational quote expert and love to write short quotes. Your mission is to generate inspirational quotes in basic HTML. Make sure to follow the user instructions.";
+  let prompt = `User instructions are: Generate an inspirational quote about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
